0428613abe1386912b0ef7598d3e8ec2a16a8c95
Abdul Samad <Samk8778@yahoo.com>
2020-08-19 22:00:34 +0530

    :tada: Initial Commit

---
+++ projbackend/controllers/paymentb.js
@@ -0,0 +1,41 @@
+var braintree = require('braintree');
+
+var gateway = braintree.connect({
+	environment: braintree.Environment.Sandbox,
+	merchantId: 'm9wfn8ffzvvt9fkk',
+	publicKey: '3xw62bftj6sb8kyr',
+	privateKey: 'ae104c798d5e5e61fa47c20d63c05ead',
+});
+
+exports.getToken = (req, res) => {
+	gateway.clientToken.generate({}, function (err, response) {
+		if (err) {
+			return res.status(500).send(err);
+		} else {
+			return res.send(response);
+		}
+	});
+};
+
+exports.processPayment = (req, res) => {
+	let nonceFromTheClient = req.body.paymentMethodNonce;
+
+	let amountFromTheClient = req.body.amount;
+	gateway.transaction.sale(
+		{
+			amount: amountFromTheClient,
+			paymentMethodNonce: nonceFromTheClient,
+
+			options: {
+				submitForSettlement: true,
+			},
+		},
+		function (err, result) {
+			if (err) {
+				return res.status(500).send(error);
+			} else {
+				return res.send(result);
+			}
+		},
+	);
+};
