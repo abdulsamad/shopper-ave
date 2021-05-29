179f108f4deec388792c245654058c40d1fd3c6c
Abdul Samad <Samk8778@yahoo.com>
2021-05-29 14:21:11 +0530

    :fire: Removed all old files to start from scratch

--- projfrontend/src/core/helper/coreapicalls.js
+++
@@ -1,9 +0,0 @@
-import { API } from '../../backend';
-
-export const getProducts = () => {
-  return fetch(`${API}/products`, { method: 'GET' })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => console.log(err));
-};
