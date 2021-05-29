4618082973010dca019ace9171e41cd776e733f9
Abdul Samad <Samk8778@yahoo.com>
2021-05-29 21:22:12 +0530

    Added Gatsby starter default

---
+++ client/gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `FaShop`,
+    description: `A few clicks is all it takes.`,
+    author: `AbdulSamad`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-offline`,
+  ],
+}
---
+++ client/src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
