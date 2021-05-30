eab55a90d5f9bb8bc6b4ae81d42e87103b346c98
Abdul Samad <Samk8778@yahoo.com>
2021-05-30 12:36:40 +0530

    :lipstick: Added navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,19 +1,68 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Header = ({ siteTitle }) => (
-  <nav className="navbar navbar-light bg-primary">
+  <nav className="navbar navbar-expand-lg navbar-light bg-primary">
     <div className="container-fluid">
-      <button className="navbar-brand btn btn-link">
-        <img
-          src="https://satyr.io/60/2"
-          alt=""
-          width="30"
-          height="24"
-          className="d-inline-block align-text-top"
+      <button className="navbar-brand btn btn-link d-flex align-items-center brand-logo-font fw-bold">
+        <StaticImage
+          src="../images/icon.png"
+          alt="FaShop"
+          width="38"
+          height="30"
         />
         {siteTitle}
       </button>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbar"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Admin Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              LogIn
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              SignUp
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              SignOut
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Cart
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 );
