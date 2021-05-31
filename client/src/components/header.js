73e2492a439e970672f2ea60a37b4628a0230cd6
Abdul Samad <Samk8778@yahoo.com>
2021-05-31 13:01:13 +0530

    :construction: Implemented auth funcitons in header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,76 +3,96 @@ import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
-const Header = ({ siteTitle }) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-    <div className="container-fluid">
-      <button className="navbar-brand btn btn-link d-inline-flex align-items-center fw-bold fs-4 brand-logo-font">
-        <StaticImage
-          src="../images/icon.png"
-          alt="FaShop"
-          width="38"
-          height="30"
-        />
-        {siteTitle}
+import { isAuthenticated, signOut } from "../auth";
+
+const Header = ({ siteTitle }) => {
+  const isLoggedIn = (
+    <li className="nav-item">
+      <button type="button" className="nav-link btn" onClick={signOut}>
+        LogOut
       </button>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbar"
-        aria-controls="navbar"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbar">
-        <ul className="navbar-nav ms-auto align-items-center">
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              activeClassName="active"
-              aria-current="page"
-              to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/">
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/">
-              Admin Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/">
-              LogIn
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/">
-              SignUp
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/">
-              SignOut
-            </Link>
-          </li>
-          <li className="nav-item mx-3">
-            <button
-              type="button"
-              className="nav-link btn btn-sm btn-outline-primary">
-              <i className="bi bi-cart4 fs-4"></i>
-            </button>
-          </li>
-        </ul>
+    </li>
+  );
+
+  const isNotLoggedIn = (
+    <>
+      <li className="nav-item">
+        <Link className="nav-link" activeClassName="active" to="/login">
+          LogIn
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" activeClassName="active" to="/signup">
+          SignUp
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <div className="container-fluid">
+        <button className="navbar-brand btn btn-link d-inline-flex align-items-center fw-bold fs-4 brand-logo-font">
+          <StaticImage
+            src="../images/icon.png"
+            alt="FaShop"
+            width={33}
+            height={30}
+          />
+          {siteTitle}
+        </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar"
+          aria-controls="navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbar">
+          <ul className="navbar-nav ms-auto align-items-center">
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                activeClassName="active"
+                aria-current="page"
+                to="/">
+                Home
+              </Link>
+            </li>
+            {isAuthenticated()?.user?.role === 0 && (
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  activeClassName="active"
+                  to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+            )}
+            {isAuthenticated()?.user?.role === 1 && (
+              <li className="nav-item">
+                <Link className="nav-link" activeClassName="active" to="/admin">
+                  Admin Dashboard
+                </Link>
+              </li>
+            )}
+            {!isAuthenticated() && isNotLoggedIn}
+            {isAuthenticated() && isLoggedIn}
+            <li className="nav-item ms-2">
+              <button type="button" className="nav-link btn">
+                <span className="d-lg-none">Cart</span>{" "}
+                <i className="bi bi-cart4 fs-4" />
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
