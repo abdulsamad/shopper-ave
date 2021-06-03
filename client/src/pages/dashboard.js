75b845f8d52e7e37605b27f4d179be26e5453ff1
Abdul Samad <Samk8778@yahoo.com>
2021-06-03 17:49:50 +0530

    :construction: Added user dashboard

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {} from "@reach/router";
 
 import Layout from "../components/layout/layout";
 import Seo from "../components/utils/SEO";
@@ -6,9 +7,28 @@ import Seo from "../components/utils/SEO";
 const UserDashboardPage = () => (
   <Layout>
     <Seo title="User Dashboard - FaShop" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <div className="container py-5">
+      <section className="card shadow-sm text-center p-2">
+        <div className="card-body">
+          <h2 className="my-2">Your User Profile Details</h2>
+          <div className="user-image my-3">
+            <img
+              src="https://picsum.photos/100"
+              alt="profile"
+              className="rounded-pill"
+            />
+          </div>
+          <div className="user-name fs-5 mb-2 mt-4">
+            <strong>Name:&nbsp;</strong>
+            <span className="fst-italic">John Doe</span>
+          </div>
+          <div className="user-email fs-5 my-2">
+            <strong>Email:&nbsp;</strong>
+            <span className="fst-italic">john@example.com</span>
+          </div>
+        </div>
+      </section>
+    </div>
   </Layout>
 );
 
