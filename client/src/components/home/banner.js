8e7340b36010184387d58c8aa7d454b9e059c570
Abdul Samad <Samk8778@yahoo.com>
2021-06-03 17:39:52 +0530

    :construction: Added banners

--- client/src/components/home/banner.js
+++ client/src/components/home/banner.js
@@ -5,7 +5,7 @@ const Banner = () => (
   <section className="banner-section">
     <div
       id="banner-carousel"
-      className="carousel slide overflow-hidden w-100 banner"
+      className="carousel slide overflow-hidden w-100 shadow-sm banner"
       data-bs-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item position-relative active first">
@@ -26,37 +26,72 @@ const Banner = () => (
             </h5>
             <p className="mb-0 fs-5">
               Upto <span className="fs-3 fw-bold fst-italic">30&#37; Off</span>{" "}
-              on all items order before the end of this month.
+              on all items ordered before the end of this month.
               <br />
               <small className="my-3 text-small">* T&amp;C Apply</small>
             </p>
-            <button className="btn btn-lg btn-primary my-3">Shop Now</button>
+            <button className="btn btn-lg btn-primary my-3">
+              Shop Now <i class="bi bi-cart4 ms-2" />
+            </button>
           </div>
         </div>
-        {/* <div className="carousel-item position-relative second">
-          <StaticImage
-            src="../../images/guy.png"
-            className="h-100"
-            imgClassName="d-block"
-            alt="shopping"
-          />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+        <div className="carousel-item second">
+          <div className="img-container position-relative d-flex justify-content-between h-100">
+            <StaticImage
+              src="../../images/cap-guy.png"
+              className="cap-guy"
+              imgClassName="d-block"
+              alt="guy in cap and jacket"
+            />
+            <StaticImage
+              src="../../images/tshirt.png"
+              className="tshirt"
+              imgClassName="d-block"
+              alt="guy in tshirt"
+            />
+            <StaticImage
+              src="../../images/hoodie.png"
+              className="hoodie"
+              imgClassName="d-block"
+              alt="guy in hoodie"
+            />
+          </div>
+          <div className="carousel-caption d-none d-md-inline-block">
+            <h5 className="display-4 fw-bold text-uppercase">
+              Ultimate Men&apos;s wear
+            </h5>
+            <p className="mb-0 fs-5 text-capitalize">
+              Be exclusive, Be Divine, Be yourself.
+            </p>
+            <button className="btn btn-lg btn-primary my-3">
+              Shop Now
+              <i class="bi bi-cart4 ms-2" />
+            </button>
           </div>
         </div>
         <div className="carousel-item position-relative third">
           <StaticImage
             src="../../images/jacket.png"
-            className="h-100"
+            className="h-100 position-absolute"
             imgClassName="d-block"
             alt="shopping"
           />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+          <div className="carousel-caption d-none d-md-inline-block">
+            <h5 className="display-3 text-uppercase fst-italic">
+              Fashion as unique as you are
+            </h5>
+            <p className="mb-0 fs-5">
+              Checkout our latest collection and get upto{" "}
+              <span className="fs-3 fw-bold fst-italic">30&#37; Off</span> on
+              all items ordered before the end of this month.
+            </p>
+            <p className="lead">Casual | Formal | Ethnic</p>
+            <button className="btn btn-lg btn-primary my-3">
+              Shop Now
+              <i class="bi bi-cart4 ms-2" />
+            </button>
           </div>
-        </div> */}
+        </div>
       </div>
       <button
         className="carousel-control-prev"
