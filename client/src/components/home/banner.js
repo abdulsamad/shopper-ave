c63aa236f022298c2d395dbd1299343140b166b0
Abdul Samad <Samk8778@yahoo.com>
2021-06-03 13:22:30 +0530

    :lipstick: Added first banner

--- client/src/components/home/banner.js
+++ client/src/components/home/banner.js
@@ -5,30 +5,58 @@ const Banner = () => (
   <section className="banner-section">
     <div
       id="banner-carousel"
-      className="carousel carousel-dark slide overflow-hidden w-100 banner"
+      className="carousel slide overflow-hidden w-100 banner"
       data-bs-ride="carousel">
       <div className="carousel-inner">
-        <div className="carousel-item active">
+        <div className="carousel-item position-relative active first">
           <StaticImage
             src="../../images/shopping.png"
-            imgClassName="d-block w-100 img-fluid"
+            className="h-100 position-absolute img-wrapper"
+            imgClassName="d-block"
             alt="shopping"
+            objectFit="cover"
           />
+          <div className="carousel-caption d-none d-md-inline-block">
+            <h5 className="display-3 text-uppercase">
+              The{" "}
+              <span className="text-primary fw-bold fst-italic">
+                festive season
+              </span>{" "}
+              is here
+            </h5>
+            <p className="mb-0 fs-5">
+              Upto <span className="fs-3 fw-bold fst-italic">30&#37; Off</span>{" "}
+              on all items order before the end of this month.
+              <br />
+              <small className="my-3 text-small">* T&amp;C Apply</small>
+            </p>
+            <button className="btn btn-lg btn-primary my-3">Shop Now</button>
+          </div>
         </div>
-        <div className="carousel-item">
+        {/* <div className="carousel-item position-relative second">
           <StaticImage
             src="../../images/guy.png"
-            imgClassName="d-block w-100 img-fluid"
+            className="h-100"
+            imgClassName="d-block"
             alt="shopping"
           />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item position-relative third">
           <StaticImage
             src="../../images/jacket.png"
-            imgClassName="d-block w-100 img-fluid"
+            className="h-100"
+            imgClassName="d-block"
             alt="shopping"
           />
-        </div>
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div> */}
       </div>
       <button
         className="carousel-control-prev"
