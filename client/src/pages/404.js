28bd42eb6950a8c3f63091b4d70fc270bf516094
Abdul Samad <Samk8778@yahoo.com>
2021-06-04 16:50:08 +0530

    :lipstick: Improved 404 page

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout/layout";
 import Seo from "../components/utils/SEO";
@@ -6,9 +8,15 @@ import Seo from "../components/utils/SEO";
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <div className="container p-5 text-center">
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container text-center">
+      <StaticImage src="../images/404.svg" height={300} className="my-5" />
+      <h1 className="mt-4">Page Not Found</h1>
+      <p className="lead">
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+      <Link to="/" className="btn btn-primary">
+        Check Homepage
+      </Link>
     </div>
   </Layout>
 );
