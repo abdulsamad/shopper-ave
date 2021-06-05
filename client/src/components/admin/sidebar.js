ba6f641ac39dfec97738a4bfd05129681c3e9d2f
Abdul Samad <Samk8778@yahoo.com>
2021-06-05 12:05:58 +0530

    :wrench: Added client side routing config

--- client/src/components/admin/sidebar.js
+++ client/src/components/admin/sidebar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "@reach/router";
 
+const isActive = ({ isCurrent }) =>
+  isCurrent
+    ? { className: "nav-link rounded-start-0 rounded-pill p-3 active" }
+    : null;
+
 const Sidebar = () => (
   <aside className="bg-l-gradient-dark fixed-bottom col-lg-2 admin-left">
     <ul className="nav nav-pills nav-justified flex-lg-column border-0 py-2">
       <li className="nav-item pe-2">
         <Link
           to="/admin/home"
-          className="nav-link rounded-start-0 rounded-pill p-3 active">
+          getProps={isActive}
+          className="nav-link rounded-start-0 rounded-pill p-3">
           <i className="bi bi-house-door fs-5 d-inline-block me-2" />
           Home
         </Link>
@@ -15,6 +21,7 @@ const Sidebar = () => (
       <li className="nav-item pe-2">
         <Link
           to="/admin/create-category"
+          getProps={isActive}
           className="nav-link rounded-start-0 rounded-pill p-3">
           <i className="bi bi-bookmark-plus fs-5 d-inline-block me-2" />
           Create Category
@@ -23,6 +30,7 @@ const Sidebar = () => (
       <li className="nav-item pe-2">
         <Link
           to="/admin/manage-categories"
+          getProps={isActive}
           className="nav-link rounded-start-0 rounded-pill p-3">
           <i className="bi bi-bookmark fs-5 d-inline-block me-2" />
           Manage Categories
@@ -31,6 +39,7 @@ const Sidebar = () => (
       <li className="nav-item pe-2">
         <Link
           to="/admin/create-product"
+          getProps={isActive}
           className="nav-link rounded-start-0 rounded-pill p-3">
           <i className="bi bi-plus-circle fs-5 d-inline-block me-2" />
           Create Product
@@ -39,6 +48,7 @@ const Sidebar = () => (
       <li className="nav-item pe-2">
         <Link
           to="/admin/manage-products"
+          getProps={isActive}
           className="nav-link rounded-start-0 rounded-pill p-3">
           <i className="bi bi-wrench fs-5 d-inline-block me-2" />
           Manage Products
@@ -47,6 +57,7 @@ const Sidebar = () => (
       <li className="nav-item pe-2">
         <Link
           to="/admin/manage-orders"
+          getProps={isActive}
           className="nav-link rounded-start-0 rounded-pill p-3">
           <i className="bi bi-cart-check fs-5 d-inline-block me-2"></i>
           Manage Orders
