19e269d5095404bc36cc3289e71d5076a2c52912
Abdul Samad <Samk8778@yahoo.com>
2021-06-06 02:26:58 +0530

    :lipstick: Added modal confirmation for deleting and more info

--- client/src/components/admin/manageCategories.js
+++ client/src/components/admin/manageCategories.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ManageCategories = () => {
   return (
-    <div className="admin-manage-categories">
+    <section className="admin-manage-categories">
       <div className="text-center">
         <h3>All Categories</h3>
         <table className="table">
@@ -27,7 +27,9 @@ const ManageCategories = () => {
               <td>
                 <button
                   type="button"
-                  className="btn btn-danger text-white w-100">
+                  className="btn btn-danger text-white w-100"
+                  data-bs-toggle="modal"
+                  data-bs-target="#delete-category-modal">
                   Delete
                 </button>
               </td>
@@ -35,7 +37,35 @@ const ManageCategories = () => {
           </tbody>
         </table>
       </div>
-    </div>
+      {/* Modal for Delete  */}
+      <div className="modal fade" tabindex="-1" id="delete-category-modal">
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">
+                Are you sure you want to delete this category?
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal">
+                No
+              </button>
+              <button type="button" className="btn btn-danger text-white">
+                Yes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- client/src/components/admin/manageProducts.js
+++ client/src/components/admin/manageProducts.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ManageProducts = () => {
   return (
-    <div className="admin-manage-products">
+    <section className="admin-manage-products">
       <div className="text-center">
         <h3>All Products</h3>
         <table className="table">
@@ -27,7 +27,9 @@ const ManageProducts = () => {
               <td>
                 <button
                   type="button"
-                  className="btn btn-danger text-white w-100">
+                  className="btn btn-danger text-white w-100"
+                  data-bs-toggle="modal"
+                  data-bs-target="#delete-product-modal">
                   Delete
                 </button>
               </td>
@@ -35,7 +37,35 @@ const ManageProducts = () => {
           </tbody>
         </table>
       </div>
-    </div>
+      {/* Modal for Delete  */}
+      <div className="modal fade" tabindex="-1" id="delete-product-modal">
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">
+                Are you sure you want to delete this product?
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal">
+                No
+              </button>
+              <button type="button" className="btn btn-danger text-white">
+                Yes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
