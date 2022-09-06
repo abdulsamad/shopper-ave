215a1c7fc24785709f4081180bc65bf5f950fcbd
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-06 19:28:44 +0530

    :wrench: Updated eslint custom global config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -3,10 +3,14 @@ const config = {
     browser: true,
     node: true,
   },
+  parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['import', '@typescript-eslint'],
+  settings: {},
   rules: {
     //
   },
+  ignorePatterns: ['**/*.js', '**/*.json', 'node_modules', '.turbo', '.next', 'public'],
 };
 
 module.exports = config;
