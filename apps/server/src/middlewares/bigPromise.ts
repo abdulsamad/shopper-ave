757413ae990529e9ee735e90bfd716d40a7cdcf6
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-19 13:58:55 +0530

    :construction: Added bigPromise helper middleware

---
+++ apps/server/src/middlewares/bigPromise.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+const bigPromise =
+  (func: (req: Request, res: Response, next: NextFunction) => void) =>
+  (req: Request, res: Response, next: NextFunction) =>
+    Promise.resolve(func(req, res, next)).catch(next);
+
+export default bigPromise;
