7b92cd55d8ecdf4e166832a523930954bad446b7
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-19 15:02:03 +0530

    :lock: Added jwt environment variables

--- apps/server/env.d.ts
+++ apps/server/env.d.ts
@@ -1,9 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
+    JWT_SECRET: string;
+    JWT_EXPIRY: string;
   }
 }
 
+// Modifying Express Request
 declare namespace Express {
   export interface Request {
     token: string;
