7c915c59244dbd1ff7c2277969661ca892a15e10
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-20 12:55:57 +0530

    :card_file_box Added database connection config

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,8 @@
 import app from './app';
+import connectDB from '@config/db';
+
+// Connect with database
+connectDB();
 
 app.listen(process.env.PORT || 8000, () => {
   // eslint-disable-next-line security-node/detect-crlf
