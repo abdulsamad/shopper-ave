decf0b054776475a628ffa794a51db3099044f33
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-20 16:31:45 +0530

    :rotating_light: Removed unused parameters

--- apps/server/src/controllers/user.ts
+++ apps/server/src/controllers/user.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 
 import User from '@models/user';
 import { respondWithCookieToken } from '@utils/respondWithCookieToken';
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
+export const signup = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
 
