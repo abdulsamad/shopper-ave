b2db1d6fb7382d0ec6fcab15807e2caadfa82985
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-20 19:41:35 +0530

    :wrench: Added moduleNameMapper in jest config

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -4,11 +4,19 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['lcov'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
   testEnvironment: 'node',
   verbose: true,
+  rootDir: 'src',
+  moduleNameMapper: {
+    '@config/(.*)': ['<rootDir>/config/$1'],
+    '@controllers/(.*)': ['<rootDir>/controllers/$1'],
+    '@middlewares/(.*)': ['<rootDir>/middlewares/$1'],
+    '@models/(.*)': ['<rootDir>/models/$1'],
+    '@routes/(.*)': ['<rootDir>/routes/$1'],
+    '@seeds/(.*)': ['<rootDir>/seeds/$1'],
+    '@utils/(.*)': ['<rootDir>/utils/$1'],
+  },
 };
 
 export default config;
