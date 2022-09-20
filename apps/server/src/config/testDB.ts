cdb93108acc54a74b42947e5811cb4acf3ff63ef
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-20 15:15:17 +0530

    :test_tube: Added testDB srcript

---
+++ apps/server/src/config/testDB.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+const mongod = MongoMemoryServer.create();
+
+export const connect = async () => {
+  const url = (await mongod).getUri();
+  const conn = await mongoose.connect(url);
+
+  return conn;
+};
+
+export const closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await (await mongod).stop();
+};
+
+export const clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+};
