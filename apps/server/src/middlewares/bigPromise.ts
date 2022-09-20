6e17cfdc55b8cf25297c9d42462fb7239e008eec
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-20 13:53:00 +0530

    :construction: Added bigPromise middleware utility

--- apps/server/src/middlewares/bigPromise.ts
+++ apps/server/src/middlewares/bigPromise.ts
@@ -1,5 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
+/**
+ * Function to get a relief from try/catch hell
+ * @param func (request, response, next: next function)
+ * @returns function which returns a proimse
+ */
 const bigPromise =
   (func: (req: Request, res: Response, next: NextFunction) => void) =>
   (req: Request, res: Response, next: NextFunction) =>
