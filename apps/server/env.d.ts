eaaf84dbaed8176596245b8c7d888449af887767
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-26 20:59:25 +0530

    :wrench: Added SMTP env vars

--- apps/server/env.d.ts
+++ apps/server/env.d.ts
@@ -2,12 +2,19 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     MONGODB_URI: string;
+    // Authentication
     JWT_SECRET: string;
     JWT_EXPIRY: string;
     COOKIE_TIME: string;
+    // Cloudinary
     CLOUDINARY_NAME: string;
     CLOUDINARY_API_KEY: string;
     CLOUDINARY_API_SECRET: string;
+    // SMTP
+    SMTP_HOST: string;
+    SMTP_PORT: string;
+    SMTP_USER: string;
+    SMTP_PASSWORD: string;
   }
 }
 
