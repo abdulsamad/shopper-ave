2b61721d301df20268994d099cc5629995c77ba7
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-26 12:54:13 +0530

    :white_check_mark: Added route test

--- apps/server/src/routes/user.test.ts
+++ apps/server/src/routes/user.test.ts
@@ -1,9 +1,15 @@
-import { getMockReq, getMockRes } from '@jest-mock/express';
+import request from 'supertest';
+import express from 'express';
 
-import { connect, clearDatabase, closeDatabase } from '@config/testDB';
+import { connect, clearDatabase, closeDatabase } from '@utils/test-db';
 import { userDetails } from '@utils/test-helpers';
-import app from '../app';
 import user from './user';
+
+// TODO: Improvise later
+const app = express();
+
+app.use(express.json());
+app.use('/', user);
 
 beforeAll(async () => await connect());
 
@@ -12,9 +18,9 @@ afterEach(async () => await clearDatabase());
 afterAll(async () => await closeDatabase());
 
 describe('User Routes', () => {
-  it('Sign Up Controller', async () => {
-    const body = app.use('/api/v1', user);
+  it('Sign Up Route', async () => {
+    const response = await request(app).post('/signup').send(userDetails);
 
-    expect(true).toEqual(true);
+    expect(response.statusCode).toBe(201);
   });
 });
