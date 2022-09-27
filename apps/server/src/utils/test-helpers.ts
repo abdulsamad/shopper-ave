2bdf7730cbc3fe06f7ed5ca9007972e0b49c2a57
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-27 10:27:37 +0530

    :white_check_mark: Updated test helpers

--- apps/server/src/utils/test-helpers.ts
+++ apps/server/src/utils/test-helpers.ts
@@ -1,11 +1,21 @@
 import { faker } from '@faker-js/faker';
 
 /* istanbul ignore file */
-export const userDetails = {
+export const user = {
+  __v: 0,
+  _id: faker.database.mongodbObjectId(),
+  email: faker.internet.email(),
+  name: faker.name.fullName(),
+  password: '🤨',
+  role: 'user',
+  createdAt: '2022-09-26T05:53:07.739Z',
+  updatedAt: '2022-09-26T05:53:07.739Z',
+};
+
+export const userDetailsForAuth = {
   name: faker.name.fullName(),
   email: faker.internet.email(),
   password: faker.internet.password(8),
-  role: 'user',
   photo: {
     id: faker.datatype.uuid(),
     secure_url: faker.internet.url(),
@@ -16,14 +26,5 @@ export const userSuccessObj = {
   success: true,
   token:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzEzZTQzY2FjNTdkN2E4M2JjN2MxMCIsImlhdCI6MTY2NDE3MTU4NywiZXhwIjoxNjY0Nzc2Mzg3fQ.EYBTuXXBfFG3SXTGnE5zKQ1M1x-R1F_7liVivlNAXYo',
-  user: {
-    __v: 0,
-    _id: faker.database.mongodbObjectId(),
-    email: faker.internet.email(),
-    name: faker.name.fullName(),
-    password: '🤨',
-    role: 'user',
-    createdAt: '2022-09-26T05:53:07.739Z',
-    updatedAt: '2022-09-26T05:53:07.739Z',
-  },
+  user,
 };
