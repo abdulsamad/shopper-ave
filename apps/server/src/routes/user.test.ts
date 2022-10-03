a26c5a6a7e30f5d5ae49233717580d3a443a3167
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-03 23:58:04 +0530

    :white_check_mark: Added more unit test for login and logout

--- apps/server/src/routes/user.test.ts
+++ apps/server/src/routes/user.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 
-import { connect, clearDatabase, closeDatabase } from '@utils/test-db';
+import { connect, closeDatabase } from '@utils/test-db';
 import { app } from '@utils/test-server';
 import { userDetailsForAuth } from '@utils/test-helpers';
 
@@ -11,12 +11,10 @@ app.use('/', user);
 
 beforeAll(async () => await connect());
 
-afterEach(async () => await clearDatabase());
-
 afterAll(async () => await closeDatabase());
 
 describe('User Routes', () => {
-  it('Sign Up Route', async () => {
+  it('Sign Up', async () => {
     const response = await request(app).post('/signup').send(userDetailsForAuth);
 
     expect(response.statusCode).toBe(201);
@@ -27,5 +25,29 @@ describe('User Routes', () => {
         user: expect.anything(),
       })
     );
-  });
+  }),
+    it('Login', async () => {
+      const { email, password } = userDetailsForAuth;
+      const response = await request(app).post('/login').send({ email, password });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          success: true,
+          token: expect.any(String),
+          user: expect.anything(),
+        })
+      );
+    }),
+    it('Logout', async () => {
+      const response = await request(app).get('/logout');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          success: true,
+          message: expect.any(String),
+        })
+      );
+    });
 });
