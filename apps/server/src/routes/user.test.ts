8aed3a8861fefa0a3ab2d0e2f282a88729623679
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-04 16:35:47 +0530

    :white_check_mark: Added forgot password route test

--- apps/server/src/routes/user.test.ts
+++ apps/server/src/routes/user.test.ts
@@ -1,10 +1,15 @@
 import request from 'supertest';
+import { faker } from '@faker-js/faker';
 
 import { connect, closeDatabase } from '@utils/test-db';
 import { app } from '@utils/test-server';
 import { userDetailsForAuth } from '@utils/test-helpers';
+import { mailHelper } from '@utils/email-helper';
 
 import user from './user';
+
+// Mocks
+jest.mock('@utils/email-helper');
 
 // Express Init
 app.use('/', user);
@@ -41,6 +46,21 @@ describe('User Routes', () => {
     }),
     it('Logout', async () => {
       const response = await request(app).get('/logout');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          success: true,
+          message: expect.any(String),
+        })
+      );
+    }),
+    it('Forgot Password', async () => {
+      const { email } = userDetailsForAuth;
+      const response = await request(app).post('/forgotpassword').send({ email });
+
+      const mockedMailHelper = <jest.Mock>mailHelper;
+      mockedMailHelper.mockImplementationOnce(() => Promise.resolve());
 
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual(
