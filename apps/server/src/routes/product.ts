472ba91ed1d714cb739bc4881fb03d5934d72203
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-17 15:21:07 +0530

    :bug: Fixed no slash in route bug

--- apps/server/src/routes/product.ts
+++ apps/server/src/routes/product.ts
@@ -26,7 +26,7 @@ router.route('/product/:id').get(getProduct);
 router.route('/admin/products').get(isLoggedIn, checkRole('admin'), adminGetAllProduct);
 router.route('/admin/product').get(isLoggedIn, checkRole('admin'), getProduct);
 router
-  .route('/admin/product:id')
+  .route('/admin/product/:id')
   .put(isLoggedIn, checkRole('admin'), adminUpdateProduct)
   .delete(isLoggedIn, checkRole('admin'), adminDeleteProduct);
 router.route('/admin/product/add').post(isLoggedIn, checkRole('admin'), addProduct);
