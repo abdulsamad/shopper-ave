8c99ba010d90a3f743e3f2df4db7107f21021db6
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-18 12:51:06 +0530

    :memo: Updated welcome route and added documentation

--- apps/server/src/controllers/home.ts
+++ apps/server/src/controllers/home.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
 
 export const welcome = (req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    message: 'Welcome to the Shopper Ave API',
-  });
+  try {
+    return res.status(200).json({
+      success: true,
+      message: 'Welcome to the Shopper Ave API',
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ err: 'Something went wrong' });
+  }
 };
