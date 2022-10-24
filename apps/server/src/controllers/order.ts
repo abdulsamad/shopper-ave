f4774990806fd11ccff656d6be849cdbeaba2ef9
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-24 12:54:44 +0530

    :bug: Fixed stock not updating while order is canceled and ordered

--- apps/server/src/controllers/order.ts
+++ apps/server/src/controllers/order.ts
@@ -22,6 +22,12 @@ export const createOrder = async (req: Request, res: Response) => {
       user: req.user?._id,
     });
 
+    await Promise.all(
+      order.orderItems.map(async ({ product, quantity }) => {
+        await updateProductstock(product, quantity);
+      })
+    );
+
     return res.status(201).json({
       success: true,
       order,
@@ -29,6 +35,40 @@ export const createOrder = async (req: Request, res: Response) => {
   } catch (err) {
     console.error();
     return res.status(500).json({ err: 'Something went wrong' });
+  }
+};
+
+/**
+ * Update the product stock quantity in the database
+ * @param productId Product's MongoDB ObjectId
+ * @param quantity Product's quantity
+ * @param increment Whether product stock should increment or decrement
+ */
+const updateProductstock = async (productId: Types.ObjectId, quantity: number, increment = false) => {
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    const stock = product.stock;
+
+    if (increment) {
+      // Increment the stock quantity (e.g Order is canceled)
+      product.stock = stock + quantity;
+    } else {
+      // Decrement the stock quantity (e.g Order is created)
+      if (stock < quantity) {
+        throw new Error('Product stock is less than required quanity');
+      }
+
+      product.stock = stock - quantity;
+    }
+
+    await product.save({ validateBeforeSave: false });
+  } catch (err) {
+    console.error(err);
   }
 };
 
@@ -111,12 +151,15 @@ export const adminUpdateOrder = async (req: Request, res: Response) => {
       return res.status(400).json({ err: 'Order is already delivered' });
     }
 
-    order.orderStatus = orderStatus;
+    if (orderStatus === 'canceled') {
+      await Promise.all(
+        order.orderItems.map(async ({ product, quantity }) => {
+          await updateProductstock(product, quantity, true);
+        })
+      );
+    }
 
-    // TODO: Check later for asynchronous concurrency issue
-    order.orderItems.forEach(async ({ product, quantity }) => {
-      await updateProductstock(product, quantity);
-    });
+    order.orderStatus = orderStatus;
 
     await order.save();
 
@@ -127,29 +170,6 @@ export const adminUpdateOrder = async (req: Request, res: Response) => {
   } catch (err) {
     console.error();
     return res.status(500).json({ err: 'Something went wrong' });
-  }
-};
-
-// Helper method to update stock on
-const updateProductstock = async (productId: Types.ObjectId, quantity: number) => {
-  try {
-    const product = await Product.findById(productId);
-
-    if (!product) {
-      throw new Error('Product not found');
-    }
-
-    const stock = product.stock;
-
-    if (stock < quantity) {
-      throw new Error('Product stock is less than required quanity');
-    }
-
-    product.stock = stock - quantity;
-
-    await product.save({ validateBeforeSave: false });
-  } catch (err) {
-    console.error(err);
   }
 };
 
@@ -166,7 +186,19 @@ export const adminDeleteOrder = async (req: Request, res: Response) => {
     const removedOrder = await order.remove();
 
     // TODO: Increase stock quantity again
-    // Product.findByIdAndUpdate(order)
+    // order.orderItems.map(async ({ product, quantity }) => {
+    //   await Product.findByIdAndUpdate(
+    //     product,
+    //     {
+    //       stock: quantity,
+    //     },
+    //     {
+    //       new: true,
+    //       runValidators: true,
+    //       useFindAndModify: false,
+    //     }
+    //   );
+    // });
 
     return res.status(200).json({
       success: true,
