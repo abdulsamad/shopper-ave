126fbaa22b93d187bf30e2f087078e1aaa93bfe3
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-25 20:07:27 +0530

    :wrench: Updated tailwind css config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['pages/**/*.{js,ts,jsx,tsx}', 'src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#388C3C',
+      secondary: '#012C01',
+      danger: '#F44336',
+      info: '#2196F3',
+    },
+    fontFamily: {
+      sans: ['Inter', 'Open Sans', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+    },
   },
-  plugins: [],
 };
