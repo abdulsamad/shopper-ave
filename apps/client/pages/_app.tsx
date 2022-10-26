717b4e4a6b7b0dd38e5873af737ac3d1c0dcdb8a
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-10-26 19:33:07 +0530

    :truck: Changed import with type alias

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 
 // Global CSS
-import '../styles/index.css';
+import '@styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
