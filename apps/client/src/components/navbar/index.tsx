28d4b37f4a251df963af42f02cfe75c77fb92260
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-11-03 20:45:59 +0530

    :construction: Added base navbar

--- apps/client/src/components/navbar/index.tsx
+++ apps/client/src/components/navbar/index.tsx
@@ -3,14 +3,29 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { ShoppingCartIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 
-import MobileNav from './MobileNav';
-
 const Index = () => {
   const [opened, setOpened] = useState(false);
 
   return (
-    <motion.nav className="" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <h1>Hello World!</h1>
+    <motion.nav
+      className="flex items-center justify-between px-12 py-4"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}>
+      <h1 className="text-2xl font-bold">ShopperAve</h1>
+      <div className="flex space-x-6">
+        <Link href="/">Home</Link>
+        <Link href="/">Collections</Link>
+      </div>
+      <div className="space-x-1">
+        <button className="p-1">
+          <ShoppingCartIcon className="h-6 w-6" />
+          <div className="sr-only">Cart</div>
+        </button>
+        <button className="p-1">
+          <UserCircleIcon className="h-6 w-6" />
+          <div className="sr-only">User Profile</div>
+        </button>
+      </div>
     </motion.nav>
   );
 };
