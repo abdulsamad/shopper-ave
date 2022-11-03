60143222ce7f102efe30eed574273e0d7f138673
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-11-03 20:59:48 +0530

    :iphone: Added separate mobile and desktop components

--- apps/client/src/components/navbar/MobileNav.tsx
+++ apps/client/src/components/navbar/MobileNav.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { ShoppingCartIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 
-const MobileNav = () => {
-  return <div>MobileNav</div>;
+interface IMobileNav {
+  opened: boolean;
+  toggleMenu: () => void;
+}
+
+const MobileNav = ({ opened, toggleMenu }: IMobileNav) => {
+  return (
+    <motion.nav
+      className="flex items-center justify-between px-12 py-4"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}>
+      <h1 className="text-2xl font-bold">ShopperAve (Mobile)</h1>
+      <div className="flex space-x-6">
+        <Link href="/">Home</Link>
+        <Link href="/">Collections</Link>
+      </div>
+      <div className="space-x-1">
+        <button className="p-1">
+          <ShoppingCartIcon className="h-6 w-6" />
+          <div className="sr-only">Cart</div>
+        </button>
+        <button className="p-1">
+          <UserCircleIcon className="h-6 w-6" />
+          <div className="sr-only">User Profile</div>
+        </button>
+      </div>
+    </motion.nav>
+  );
 };
 
 export default MobileNav;
--- apps/client/src/components/navbar/index.tsx
+++ apps/client/src/components/navbar/index.tsx
@@ -1,33 +1,22 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
-import { ShoppingCartIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+import useMediaQuery from 'beautiful-react-hooks/useMediaQuery';
+
+import DesktopNav from './DesktopNav';
+import MobileNav from './MobileNav';
 
 const Index = () => {
+  const isMobile = useMediaQuery('(max-width: 768px)');
   const [opened, setOpened] = useState(false);
 
-  return (
-    <motion.nav
-      className="flex items-center justify-between px-12 py-4"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}>
-      <h1 className="text-2xl font-bold">ShopperAve</h1>
-      <div className="flex space-x-6">
-        <Link href="/">Home</Link>
-        <Link href="/">Collections</Link>
-      </div>
-      <div className="space-x-1">
-        <button className="p-1">
-          <ShoppingCartIcon className="h-6 w-6" />
-          <div className="sr-only">Cart</div>
-        </button>
-        <button className="p-1">
-          <UserCircleIcon className="h-6 w-6" />
-          <div className="sr-only">User Profile</div>
-        </button>
-      </div>
-    </motion.nav>
-  );
+  const toggleMenu = () => {
+    setOpened((open) => !open);
+  };
+
+  if (isMobile) {
+    return <MobileNav opened={opened} toggleMenu={toggleMenu} />;
+  }
+
+  return <DesktopNav />;
 };
 
 export default Index;
