4ec2916c5c42e94cfc942dd365c62d88fdf83f2a
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-11-04 23:20:56 +0530

    :iphone: Added hamburger button on mobile

--- apps/client/src/components/navbar/MobileNav.tsx
+++ apps/client/src/components/navbar/MobileNav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import { ShoppingCartIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 interface IMobileNav {
   opened: boolean;
@@ -11,22 +11,17 @@ interface IMobileNav {
 const MobileNav = ({ opened, toggleMenu }: IMobileNav) => {
   return (
     <motion.nav
-      className="flex items-center justify-between px-12 py-4"
+      className="flex items-center justify-between px-4 py-4"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}>
       <h1 className="text-2xl font-bold">ShopperAve (Mobile)</h1>
-      <div className="flex space-x-6">
+      <div className="hidden space-x-6">
         <Link href="/">Home</Link>
         <Link href="/">Collections</Link>
       </div>
       <div className="space-x-1">
         <button className="p-1">
-          <ShoppingCartIcon className="h-6 w-6" />
-          <div className="sr-only">Cart</div>
-        </button>
-        <button className="p-1">
-          <UserCircleIcon className="h-6 w-6" />
-          <div className="sr-only">User Profile</div>
+          <Bars3Icon className="h-6 w-6" />
         </button>
       </div>
     </motion.nav>
