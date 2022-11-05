91fed8d1a9cb19ed32667c7415a1ef4caed18a8b
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-11-05 11:34:33 +0530

    :wrench: Added screen breakpoints in tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -20,6 +20,13 @@ module.exports = {
       sans: ['Inter', 'Open Sans', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       spacing: {
         128: '32rem',
