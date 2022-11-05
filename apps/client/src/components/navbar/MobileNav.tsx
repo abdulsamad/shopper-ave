02b14c338947f518ae468b8dc13af07d2e619dbd
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-11-05 12:36:56 +0530

    :dizzy: Added dropdown animation

--- apps/client/src/components/navbar/MobileNav.tsx
+++ apps/client/src/components/navbar/MobileNav.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
 
 interface IMobileNav {
   opened: boolean;
   toggleMenu: () => void;
 }
+
+const dropdownVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const DropdownItemVariant: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+  },
+};
 
 const MobileNav = ({ opened, toggleMenu }: IMobileNav) => {
   return (
@@ -14,10 +36,23 @@ const MobileNav = ({ opened, toggleMenu }: IMobileNav) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}>
       <h1 className="text-2xl font-bold">ShopperAve</h1>
-      <div className="bg-primary absolute top-[60px] left-1/2 flex w-full -translate-x-1/2 flex-col space-x-6 p-5">
-        <Link href="/">Home</Link>
-        <Link href="/">Collections</Link>
-      </div>
+      <AnimatePresence>
+        {opened && (
+          <motion.ul
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            variants={dropdownVariants}
+            className="absolute top-[60px] left-1/2 flex w-full -translate-x-1/2 flex-col items-center bg-white shadow-md">
+            <motion.li className="w-full pt-2 text-center" variants={DropdownItemVariant}>
+              <Link href="/">Home</Link>
+            </motion.li>
+            <motion.li className="w-full py-2 text-center" variants={DropdownItemVariant}>
+              <Link href="/collection">Collection</Link>
+            </motion.li>
+          </motion.ul>
+        )}
+      </AnimatePresence>
       <div className="overflow-hidden">
         <button className="h-100 w-100 p-3" onClick={toggleMenu}>
           <div className="space-y-[5px]">
@@ -30,7 +65,7 @@ const MobileNav = ({ opened, toggleMenu }: IMobileNav) => {
               className="h-[1.5px] w-5 rounded-full bg-black"
             />
             <motion.div
-              animate={{ opacity: opened ? 0 : 1 }}
+              animate={{ opacity: opened ? 0 : 1, scaleX: opened ? 0 : 1 }}
               className="h-[1.5px] w-5 rounded-full bg-black"
             />
             <motion.div
