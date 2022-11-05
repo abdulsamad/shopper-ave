071d432649f4fadc0a0eee492bae32385655112a
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-11-05 12:10:14 +0530

    :dizzy: Added hamburger animation

--- apps/client/src/components/navbar/MobileNav.tsx
+++ apps/client/src/components/navbar/MobileNav.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import { Bars3Icon } from '@heroicons/react/24/outline';
 
 interface IMobileNav {
   opened: boolean;
@@ -14,14 +13,35 @@ const MobileNav = ({ opened, toggleMenu }: IMobileNav) => {
       className="flex items-center justify-between px-4 py-4"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}>
-      <h1 className="text-2xl font-bold">ShopperAve (Mobile)</h1>
-      <div className="hidden space-x-6">
+      <h1 className="text-2xl font-bold">ShopperAve</h1>
+      <div className="bg-primary absolute top-[60px] left-1/2 flex w-full -translate-x-1/2 flex-col space-x-6 p-5">
         <Link href="/">Home</Link>
         <Link href="/">Collections</Link>
       </div>
-      <div className="space-x-1">
-        <button className="p-1">
-          <Bars3Icon className="h-6 w-6" />
+      <div className="overflow-hidden">
+        <button className="h-100 w-100 p-3" onClick={toggleMenu}>
+          <div className="space-y-[5px]">
+            <motion.div
+              animate={{
+                rotateZ: opened ? '45deg' : '0deg',
+                translateY: opened ? 6.5 : 0,
+                transition: { delay: 0.1 },
+              }}
+              className="h-[1.5px] w-5 rounded-full bg-black"
+            />
+            <motion.div
+              animate={{ opacity: opened ? 0 : 1 }}
+              className="h-[1.5px] w-5 rounded-full bg-black"
+            />
+            <motion.div
+              animate={{
+                rotateZ: opened ? '-45deg' : '0deg',
+                translateY: opened ? -6.5 : 0,
+                transition: { delay: 0.25 },
+              }}
+              className="h-[1.5px] w-5 rounded-full bg-black"
+            />
+          </div>
         </button>
       </div>
     </motion.nav>
