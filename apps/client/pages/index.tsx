92d4aadaf7590cfbf70aa05449898fc8827ecf8b
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-05 23:11:26 +0530

    :construction: Updated index page

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 
 import Banner from '@components/banner';
 import Sidebar from '@components/sidebar';
+import Products from '@components/products';
 
 const Home: NextPage = () => {
   return (
@@ -12,16 +13,16 @@ const Home: NextPage = () => {
         <meta name="description" content="An E-Commerce store built with MERN stack" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="grid grid-cols-4 place-items-center">
+      <main className="grid grid-cols-4">
         <section className="col-span-4">
           <Banner />
         </section>
-        <section className="col-span-4 flex h-[60px] w-full">
+        <section className="col-span-4 flex h-[60px] w-full bg-cyan-400">
           <div className="flex w-full items-center justify-center">Ad</div>
         </section>
         <Sidebar />
-        <section className="bg-info col-span-3 h-[500px] w-full">
-          <div className="flex h-full w-full items-center justify-center">Product</div>
+        <section className="col-span-3">
+          <Products />
         </section>
       </main>
     </div>
