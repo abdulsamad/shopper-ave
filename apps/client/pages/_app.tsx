5c1e0dd67bd63a6b8c5d2fa7e77e8b5b9ad4206c
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-05 22:49:14 +0530

    :construction: Added queryClientProvider react

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Navbar from '@components/navbar';
 import Footer from '@components/footer';
@@ -7,12 +9,14 @@ import Footer from '@components/footer';
 import '@styles/index.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </div>
+    </QueryClientProvider>
   );
 };
 
