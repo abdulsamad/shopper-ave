87f2afa6a344ace1a2166c4cbe4e3cdb5d585ea7
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-12 23:59:47 +0530

    :construction: Added register and login page

---
+++ apps/client/pages/register.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
+
+import Input from '@utils/Input';
+import Button from '@utils/Button';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+const Register: NextPage = () => {
+  const { handleSubmit, control } = useForm<Inputs>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  return (
+    <section className="my-5">
+      <div className="mx-auto max-w-full px-5 md:w-[500px]">
+        <form
+          className=""
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}>
+          <Input
+            type="email"
+            label="Email"
+            placeholder="name@example.com"
+            id="email"
+            control={control}
+          />
+          <Input
+            type="password"
+            label="Password"
+            placeholder="Enter password"
+            id="password"
+            control={control}
+          />
+          <Button className="bg-primary hover:bg-primary-500 mt-2 px-4 py-2 text-white">
+            Create Account
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
