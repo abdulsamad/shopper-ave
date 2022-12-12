9ad6564ba3c0ef0203f6604c3264080fefcecdf9
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-12 23:07:16 +0530

    :wrench: Updated primary shades in tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#388C3C',
+        primary: {
+          50: '#f3faf3',
+          100: '#e3f5e3',
+          200: '#c8eac9',
+          300: '#9dd89f',
+          400: '#6abe6e',
+          500: '#46a14a',
+          600: '#388c3c',
+          700: '#2c692f',
+          800: '#27542a',
+          900: '#224525',
+          dark: '#224525',
+          light: '#9dd89f',
+          DEFAULT: '#388c3c',
+        },
         secondary: '#012C01',
         danger: '#F44336',
         info: '#2196F3',
