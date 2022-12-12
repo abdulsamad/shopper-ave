f8f95fcbb899cb17b266b6e26aa85e49b7284e1e
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-12 23:55:14 +0530

    :construction: Updated input component

--- apps/client/src/utils/Input.tsx
+++ apps/client/src/utils/Input.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import { Controller, Control } from 'react-hook-form';
 
 interface InputProps {
   label: string;
   placeholder: string;
+  id: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any, any>;
   type?: string;
-  id?: string;
   className?: string;
 }
 
-const Input = ({ label, placeholder, type = 'text', id, className, ...props }: InputProps) => {
+const Input = ({ label, placeholder, type = 'text', id, className, control }: InputProps) => {
   return (
     <div className="my-2 flex flex-col">
       <label htmlFor={id} className="mb-2 font-medium">
         {label}
       </label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        id={id}
-        className={`py-2 px-4 ${className ?? 'rounded-xl bg-gray-100 shadow'}`}
-        {...props}
+      <Controller
+        name={id}
+        render={({ field }) => (
+          <input
+            type={type}
+            placeholder={placeholder}
+            className={`py-2 px-4 ${className ?? 'rounded-xl bg-gray-100 shadow'}`}
+            {...field}
+          />
+        )}
+        control={control}
       />
     </div>
   );
