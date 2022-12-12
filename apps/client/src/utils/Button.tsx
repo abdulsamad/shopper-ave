9e5d7aabb49705fdebfe3ed8b5334156feb395dd
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-12 23:08:12 +0530

    :construction: Removed unwanted props

--- apps/client/src/utils/Button.tsx
+++ apps/client/src/utils/Button.tsx
@@ -2,36 +2,34 @@ import React, { MouseEventHandler } from 'react';
 import Link from 'next/link';
 
 interface ButtonProps {
-  color: string;
   children: React.ReactNode;
   onClick?: MouseEventHandler;
   type?: 'button' | 'submit' | 'reset';
   className?: string;
 }
 
-export const Button = ({ color, children, onClick, type, className }: ButtonProps) => (
+export const Button = ({ children, onClick, type, className }: ButtonProps) => (
   <button
     type={type}
     onClick={onClick}
-    className={`bg-${color} hover:bg-${color}-500 items-center rounded-lg py-1.5 px-3.5 text-sm font-semibold lg:inline-flex ${className}`}>
+    className={`items-center rounded-lg py-1.5 px-3.5 text-sm font-semibold lg:inline-flex ${className}`}>
     {children}
   </button>
 );
 
 interface LinkButtonProps {
   href: string;
-  color: string;
   children: React.ReactNode;
   onClick?: MouseEventHandler;
   type?: 'button' | 'submit' | 'reset';
   className?: string;
 }
 
-export const LinkButton = ({ href, onClick, color, className, children }: LinkButtonProps) => (
+export const LinkButton = ({ href, children, onClick, className }: LinkButtonProps) => (
   <Link
     href={href}
     onClick={onClick}
-    className={`bg-${color} hover:bg-${color}-500 items-center rounded-lg py-1.5 px-3.5 text-sm font-semibold lg:inline-flex ${className}`}>
+    className={`items-center rounded-lg py-1.5 px-3.5 text-sm font-semibold lg:inline-flex ${className}`}>
     {children}
   </Link>
 );
