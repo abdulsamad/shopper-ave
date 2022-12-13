8237062339f03f3fd1015842607ead8789411949
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-13 11:42:33 +0530

    :dizzy: Added shake animation in tailwind config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -42,6 +42,25 @@ module.exports = {
         128: '32rem',
         144: '36rem',
       },
+      animation: {
+        shake: 'shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both',
+      },
+      keyframes: {
+        shake: {
+          '10%, 90%': {
+            transform: 'translate3d(-1px, 0 , 0)',
+          },
+          '20%, 80%': {
+            transform: 'translate3d(2px, 0 , 0)',
+          },
+          '30%, 50%, 70%': {
+            transform: 'translate3d(-3px, 0 , 0)',
+          },
+          '40%, 60%': {
+            transform: 'translate3d(3px, 0, 0)',
+          },
+        },
+      },
     },
   },
 };
