b327403d9ebcf535ac2fa68cf979d69a4093d1a1
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-13 20:04:34 +0530

    :construction: Updated banner

--- apps/client/src/components/user/banner/index.tsx
+++ apps/client/src/components/user/banner/index.tsx
@@ -5,13 +5,20 @@ import AutoPlay from 'embla-carousel-autoplay';
 
 const bannerImages = [
   {
-    src: 'https://res.cloudinary.com/abdulsamad/image/upload/v1666595994/products/dlr157h1tx8pfbm97gug.jpg',
+    src: 'https://res.cloudinary.com/abdulsamad/image/upload/v1670939764/shopping_xgna10.webp',
+    text: (
+      <h1 className="absolute top-1/2 left-5 bg-amber-200 text-center text-3xl">
+        The <span>Festive Season</span> is HERE.
+      </h1>
+    ),
   },
   {
-    src: 'https://res.cloudinary.com/abdulsamad/image/upload/v1670931462/products/pf1rlaxbezpy1ntygwfo.jpg',
-  },
-  {
-    src: 'https://res.cloudinary.com/abdulsamad/image/upload/v1666515085/products/muxx1yzmsbeyhysxzzyh.jpg',
+    src: 'https://res.cloudinary.com/abdulsamad/image/upload/v1670939761/jacket_inlgnr.webp',
+    text: (
+      <h1 className="absolute top-1/2 left-5 bg-amber-200 text-center text-3xl uppercase">
+        Fashion as unique as you are
+      </h1>
+    ),
   },
 ];
 
@@ -21,15 +28,16 @@ const Index = () => {
   return (
     <div className="embla h-[250px] w-full overflow-hidden lg:h-[500px]" ref={emblaRef}>
       <div className="embla__container flex h-full">
-        {bannerImages.map(({ src }) => (
-          <div key={src} className="embla__slide shrink-0 grow-0 basis-full">
+        {bannerImages.map(({ src, text }) => (
+          <div key={src} className="embla__slide relative h-full shrink-0 grow-0 basis-full">
             <div className="flex h-full items-center justify-center">
+              {text}
               <Image
                 src={src}
-                className="object-cover"
+                className="object-contain"
                 alt="Banner"
-                height={500}
-                width={1920}
+                height={800}
+                width={800}
                 priority
               />
             </div>
