2e642f2bbce644e925b2f81e6108118bdb7d8563
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-01-10 23:31:41 +0530

    :construction: Added format currency utility

---
+++ apps/client/src/utils/index.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (price: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  }).format(price);
+};
