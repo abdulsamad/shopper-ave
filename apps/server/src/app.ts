623aa88a45c50c1d06e8256cb80f730182d0524d
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-20 12:32:34 +0530

    :bulb: Updated comment

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -18,7 +18,7 @@ dotenv.config();
 
 const app = express();
 
-// Allow Resource Origin
+// Allow Resource Origin Locally
 if (process.env.NODE_ENV !== 'production') {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
