aa9d8dece1a075809254e561dd4cce9c803aa17a
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-20 14:42:14 +0530

    :lipstick: Delete duplicate components

--- apps/client/src/components/user/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = () => (
-  <footer className="container mx-auto py-4">
-    <h6 className="text-center">ShopperAve Inc. &copy; 2022</h6>
-  </footer>
-);
-
-export default Index;
