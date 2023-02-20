73a34125dff12de0b06acb30e59e457802519cd8
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-20 12:31:23 +0530

    :wrench: Added allow methods and authorization in allow headers

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -22,7 +22,11 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
 
     next();
   });
