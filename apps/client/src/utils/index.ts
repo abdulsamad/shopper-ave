aa9647d492f9ade5537b76306b274f2fc7bdb98b
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-21 15:39:24 +0530

    :pencil2: Added form data helper

--- apps/client/src/utils/index.ts
+++ apps/client/src/utils/index.ts
@@ -9,3 +9,28 @@ export const formatCurrency = (price: number) => {
 export const formatNumber = (price: number) => {
   return new Intl.NumberFormat('en-US').format(price);
 };
+
+export const createFormData = (data: object) => {
+  const formData = new FormData();
+
+  for (const [formKey, val] of Object.entries(data)) {
+    if (!val) continue;
+
+    // File
+    if (typeof val === 'object' && val.constructor.name.toLowerCase() === 'filelist') {
+      for (let i = 0; i < val.length; i++) {
+        // Confirm it's a file blob
+        if (val[i] instanceof Blob) {
+          formData.append(formKey, val[i]);
+        }
+      }
+
+      continue;
+    }
+
+    // Other Fields
+    if (typeof val === 'string') formData.append(formKey, val);
+  }
+
+  return formData;
+};
