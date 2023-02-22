bbb8cb751cfb7c6463446a0c4230f4e227836ff6
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-22 13:54:18 +0530

    :wrench: Fixed wrong attribute value in document

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -5,7 +5,7 @@ function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&family=Merriweather:wght@400;700&display=swap"
