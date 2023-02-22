5e7a12ee5a3917b8552a71f38a4d2f1278039077
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-22 18:08:12 +0530

    :lipstick: Added isActive class

--- apps/client/src/components/admin/sidebar/index.tsx
+++ apps/client/src/components/admin/sidebar/index.tsx
@@ -44,19 +44,21 @@ const sidebarItems = [
 const Sidebar = () => {
   const router = useRouter();
 
-  console.log(router);
-
   return (
     <aside className="bg-primary-600 col-span-2 text-white">
       <ul>
-        {sidebarItems.map(({ title, Icon, route }) => (
-          <li key={title}>
-            <Link href={`/admin/${route}`} className="flex items-center justify-center px-4 py-5">
-              {<Icon className="mr-2 h-5 w-5" />}
-              <span>{title}</span>
-            </Link>
-          </li>
-        ))}
+        {sidebarItems.map(({ title, Icon, route }) => {
+          const isActive = router.pathname === `/admin/${route}`;
+
+          return (
+            <li key={title} className={isActive ? 'bg-red-500' : ''}>
+              <Link href={`/admin/${route}`} className="flex items-center justify-center px-4 py-5">
+                {<Icon className="mr-2 h-5 w-5" />}
+                <span>{title}</span>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
