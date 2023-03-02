0a2c3ab0a41e7f822ca42ca684dfe85c029eab52
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-02 21:03:58 +0530

    :truck: Moved navbar and footer to components

--- apps/client/src/components/footer/index.tsx
+++ apps/client/src/components/footer/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Index = () => (
-  <footer className="container mx-auto py-4">
+  <footer className="container mx-auto flex-grow-0 py-4">
     <h6 className="text-center">ShopperAve Inc. &copy; 2022</h6>
   </footer>
 );
