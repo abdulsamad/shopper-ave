b39ef91d0b6a4ab832b346baf69e4954f7730ca1
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-03 19:16:00 +0530

    :construction: Added cors default origin

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // Allow Resource Origin Locally
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN);
+  res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
