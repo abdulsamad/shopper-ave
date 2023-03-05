daf5a6f9b051add28252e8bde94bdedc034271cf
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-05 18:28:50 +0530

    :construction: Added stripe api calls

--- apps/client/src/api/user.ts
+++ apps/client/src/api/user.ts
@@ -2,12 +2,6 @@ import { Product } from 'shared-types';
 
 import { axiosInstance } from './axiosInstance';
 
-export const getStripeAPIKey = async (): Promise<{ key: string }> => {
-  const res = await axiosInstance.get('/payment/stripekey');
-  const data = await res.data;
-  return data;
-};
-
 export interface IGetProductsRes {
   success: boolean;
   products: Product[];
@@ -26,6 +20,28 @@ export interface IGetProductRes {
 
 export const getProduct = async (id: string): Promise<IGetProductRes> => {
   const res = await axiosInstance.get(`/product/${id}`);
+  const data = await res.data;
+  return data;
+};
+
+export const getStripeAPIKey = async (): Promise<{ key: string }> => {
+  const res = await axiosInstance.get('/payment/stripekey');
+  const data = await res.data;
+  return data;
+};
+
+export interface ICapturePaymenRes {
+  success: boolean;
+  client_secret: string;
+  amount: number;
+}
+
+export const capturePayment = async (amount: number): Promise<ICapturePaymenRes> => {
+  const res = await axiosInstance.post(
+    '/payment/capturestripe',
+    { amount },
+    { headers: { 'Content-Type': 'application/json' } }
+  );
   const data = await res.data;
   return data;
 };
