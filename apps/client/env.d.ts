3a9967a98b1688c6a2e59c760a5d61ee70e99ea5
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-05 17:49:54 +0530

    :construction: Updated stripe key name

--- apps/client/env.d.ts
+++ apps/client/env.d.ts
@@ -2,6 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_URL: string;
     // Stripe
-    NEXT_PUBLIC_STRIPE_API_KEY: string;
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
   }
 }
