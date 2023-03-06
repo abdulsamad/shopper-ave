b8ea39986ae50b4750e84fa672182879a96ba025
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-06 17:48:30 +0530

    :construction: Added cart store on product listing

--- apps/client/src/components/user/products/Item.tsx
+++ apps/client/src/components/user/products/Item.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { Product } from 'shared-types';
+import { Product as IProduct } from 'shared-types';
 
 import { formatCurrency } from '@utils/index';
+import { useCart } from '@store/index';
+import Button from '@utils/Button';
 
-const Item = ({ _id, name, category, photos, price }: Product) => {
+const Item = (product: IProduct) => {
+  const { _id, name, category, photos, price } = product;
+  const { actions } = useCart();
+
   return (
     <div className="flex h-full flex-col justify-between overflow-hidden rounded-2xl shadow">
       <Link className="flex h-full flex-col" href={`/product/${encodeURIComponent(_id)}`}>
@@ -24,18 +29,23 @@ const Item = ({ _id, name, category, photos, price }: Product) => {
         </div>
       </Link>
       <div className="flex">
-        <button
+        <Button
           type="button"
-          onClick={(ev) => ev.stopPropagation()}
+          onClick={(ev) => {
+            ev.stopPropagation();
+          }}
           className="w-1/2 bg-gradient-to-r from-cyan-300 to-blue-400 p-2 text-white">
           Buy Now
-        </button>
-        <button
+        </Button>
+        <Button
           type="button"
-          onClick={(ev) => ev.stopPropagation()}
+          onClick={(ev) => {
+            ev.stopPropagation();
+            actions.add(product);
+          }}
           className="from-primary-400 to-primary-600 w-1/2 bg-gradient-to-r p-2 text-white">
           Add to Cart
-        </button>
+        </Button>
       </div>
     </div>
   );
