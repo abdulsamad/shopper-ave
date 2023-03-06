9756bec3cf6c4edc9466a67f1fd721814241d1d2
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-06 22:18:57 +0530

    :construction: Added domain env var

--- apps/client/env.d.ts
+++ apps/client/env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_DOMAIN: string;
     // Stripe
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
   }
