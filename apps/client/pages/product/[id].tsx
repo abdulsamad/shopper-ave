443d0c372355173118c998c95e39f87e1e68731b
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-06 21:29:33 +0530

    :bug: Fixed product page height not extended

--- apps/client/pages/product/[id].tsx
+++ apps/client/pages/product/[id].tsx
@@ -16,39 +16,41 @@ const Index: NextPage<IProduct> = (product: IProduct) => {
   const { actions } = useCart();
 
   return (
-    <div className="grid grid-cols-2 place-items-center p-5">
-      <Photos photos={photos} name={name} />
-      <section className="flex flex-col gap-6">
-        <div>
-          <h2 className="text-xl font-bold">{brand}</h2>
-          <h1 className="mt-3 text-3xl font-light">{name}</h1>
-          <div className="mb-2 flex">
-            {[1, 2, 3, 4, 5].map((star) =>
-              star <= ratings ? (
-                <StarIcon key={star} className="h-4 w-4 text-yellow-500" />
-              ) : (
-                <StarIconOutline key={star} className="h-4 w-4" />
-              )
-            )}
+    <div className="flex-1">
+      <div className="grid grid-cols-2 place-items-center p-5">
+        <Photos photos={photos} name={name} />
+        <section className="flex flex-col gap-6">
+          <div>
+            <h2 className="text-xl font-bold">{brand}</h2>
+            <h1 className="mt-3 text-3xl font-light">{name}</h1>
+            <div className="mb-2 flex">
+              {[1, 2, 3, 4, 5].map((star) =>
+                star <= ratings ? (
+                  <StarIcon key={star} className="h-4 w-4 text-yellow-500" />
+                ) : (
+                  <StarIconOutline key={star} className="h-4 w-4" />
+                )
+              )}
+            </div>
+            <p className="m-0">{category}</p>
+            <p className="text my-2 text-xl font-bold capitalize">{formatCurrency(price)}</p>
+            <p className="">{description}</p>
           </div>
-          <p className="m-0">{category}</p>
-          <p className="text my-2 text-xl font-bold capitalize">{formatCurrency(price)}</p>
-          <p className="">{description}</p>
-        </div>
-        <div className="flex gap-6">
-          <Button type="button" className="bg-primary text-white">
-            <ShoppingBagIcon className="mr-3 h-5 w-5" /> Buy Now
-          </Button>
-          <Button
-            type="button"
-            className="border-primary text-primary hover:bg-primary border border-solid py-2 transition duration-300 ease-out hover:text-white"
-            onClick={() => {
-              actions.add(product);
-            }}>
-            <ShoppingCartIcon className="mr-3 h-5 w-5" /> Add to Cart
-          </Button>
-        </div>
-      </section>
+          <div className="flex gap-6">
+            <Button type="button" className="bg-primary text-white">
+              <ShoppingBagIcon className="mr-3 h-5 w-5" /> Buy Now
+            </Button>
+            <Button
+              type="button"
+              className="border-primary text-primary hover:bg-primary border border-solid py-2 transition duration-300 ease-out hover:text-white"
+              onClick={() => {
+                actions.add(product);
+              }}>
+              <ShoppingCartIcon className="mr-3 h-5 w-5" /> Add to Cart
+            </Button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
