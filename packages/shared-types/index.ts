0809ece865d3822d62b48201377e2fb57ab6716e
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-08 12:41:18 +0530

    :label: Added order types

--- packages/shared-types/index.ts
+++ packages/shared-types/index.ts
@@ -1,4 +1,5 @@
 import type { User } from './src/user';
 import type { Product, Review, Photo } from './src/product';
+import type { Order, OrderItem, PaymentInfo, ShippingInfo } from './src/order';
 
-export type { User, Product, Review, Photo };
+export type { User, Product, Review, Photo, Order, OrderItem, PaymentInfo, ShippingInfo };
