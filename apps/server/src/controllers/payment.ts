f3859ed8c6eb062e3c233165c61503b37037ec5e
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-08 18:09:10 +0530

    :bug: Fixed payment calculation bug

--- apps/server/src/controllers/payment.ts
+++ apps/server/src/controllers/payment.ts
@@ -29,7 +29,7 @@ export const capturePayment = async (req: Request, res: Response) => {
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount / 100,
+      amount: amount * 100,
       currency: 'INR',
 
       // Optional metadata for payment on stripe
