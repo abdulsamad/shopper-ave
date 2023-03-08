9affe9b3ebc8fe6309d9c2b3f80f31b068c25706
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-08 12:49:44 +0530

    :construction: Updated maximum fractional digits

--- apps/client/src/utils/index.ts
+++ apps/client/src/utils/index.ts
@@ -2,7 +2,7 @@ export const formatCurrency = (price: number) => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 0,
+    minimumFractionDigits: 2,
   }).format(price);
 };
 
