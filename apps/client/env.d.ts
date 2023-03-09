ba53e1720a2e598823fae6cbbe02202f17ca4091
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-09 16:34:40 +0530

    :hammer: Added env var for test account

--- apps/client/env.d.ts
+++ apps/client/env.d.ts
@@ -2,6 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_URL: string;
     NEXT_PUBLIC_DOMAIN: string;
+    NEXT_PUBLIC_TEST_EMAIL: string;
+    NEXT_PUBLIC_TEST_PASSWORD: string;
     // Stripe
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
   }
