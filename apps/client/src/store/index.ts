a4fd0aec58aaeab8b4ffc9d24592daffff1dc994
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-09 16:35:13 +0530

    :bug: Added custom hook for store hydration error in persist

--- apps/client/src/store/index.ts
+++ apps/client/src/store/index.ts
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import { useAuthStore } from './auth';
 import { useCartStore } from './cart';
+
+/* Custom hook for persisted store / hydration error */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useStore = <T, F>(
+  store: (callback: (state: T) => unknown) => unknown,
+  callback: (state: T) => F
+) => {
+  const result = store(callback) as F;
+  const [data, setData] = useState<F>();
+
+  useEffect(() => {
+    setData(result);
+  }, [result]);
+
+  return data;
+};
 
 // Auth
 export const useAuthActions = () => useAuthStore((state) => state.actions);
