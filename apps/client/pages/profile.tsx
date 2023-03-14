7a3c8ce043c741cdb93e6f41241db310c1a21b98
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-14 19:46:53 +0530

    :lipstick: Updated profile page

--- apps/client/pages/profile.tsx
+++ apps/client/pages/profile.tsx
@@ -1,23 +1,12 @@
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import dayjs from 'dayjs';
-import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
 
-import { useAuthActions, useUser } from '@store/index';
-import Button from '@utils/Button';
+import { useUser } from '@store/index';
 
 const ProfilePage: NextPage = () => {
   const user = useUser();
-  const actions = useAuthActions();
-  const router = useRouter();
-
-  useLayoutEffect(() => {
-    if (!user) {
-      router.push('/');
-    }
-  }, [user, router]);
 
   if (!user) return null;
 
@@ -26,7 +15,7 @@ const ProfilePage: NextPage = () => {
   return (
     <section className="my-5 flex flex-1 flex-col items-center justify-center">
       <div className="mb-16">
-        {user.photo && (
+        {user.photo ? (
           <Image
             width={300}
             height={300}
@@ -34,6 +23,10 @@ const ProfilePage: NextPage = () => {
             alt={name}
             className="rounded-full object-cover"
           />
+        ) : (
+          <div className="bg-primary-200 flex h-[150px] w-[150px] items-center justify-center overflow-hidden rounded-full">
+            <div className="text-6xl">{name.substring(0, 1)}</div>
+          </div>
         )}
       </div>
       <div className="flex flex-col space-y-4 text-center">
@@ -53,10 +46,6 @@ const ProfilePage: NextPage = () => {
           <span className="mr-2 font-semibold">Created On:</span>
           <span>{dayjs(createdAt).format('DD MMM YYYY')}</span>
         </div>
-        <Button className="bg-slate-100" onClick={() => actions.logout()}>
-          <ArrowLeftOnRectangleIcon className="mr-2 h-6 w-6" />
-          Logout
-        </Button>
       </div>
     </section>
   );
