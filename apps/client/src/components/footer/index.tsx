d845a5ac55511522fb5108e79f4365b8b18f9849
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-15 19:48:24 +0530

    :construction: Updated footer year

--- apps/client/src/components/footer/index.tsx
+++ apps/client/src/components/footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Index = () => (
   <footer className="container mx-auto flex-grow-0 py-4">
-    <h6 className="text-center">ShopperAve Inc. &copy; 2022</h6>
+    <h6 className="text-center">ShopperAve Inc. &copy; 2023</h6>
   </footer>
 );
 
