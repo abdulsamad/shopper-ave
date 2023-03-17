4313e56894fd300b0143d2d6406cfcab2600850f
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-18 03:54:14 +0530

    :construction: Fixed reivew UI

--- apps/client/src/components/user/review/index.tsx
+++ apps/client/src/components/user/review/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -8,6 +8,7 @@ import { StarIcon as ActiveStarIcon } from '@heroicons/react/24/solid';
 
 import { Review } from 'shared-types';
 
+import { useUser } from '@store/index';
 import { addReview } from '@api/user';
 import TextArea from '@utils/TextArea';
 import Button from '@utils/Button';
@@ -27,7 +28,11 @@ interface IProps {
 }
 
 const Index = ({ productId, reviews }: IProps) => {
+  const user = useUser();
   const [active, setActive] = useState(true);
+  const [userCanReview, setUserCanReview] = useState(() =>
+    reviews.findIndex((item) => item.user !== user?._id)
+  );
   const {
     handleSubmit,
     control,
@@ -68,6 +73,7 @@ const Index = ({ productId, reviews }: IProps) => {
         });
 
         reset();
+        setUserCanReview(0);
       } catch (err) {
         if (isAxiosError(err) && err.response)
           setError('root', { type: 'custom', message: err.response.data.err });
@@ -79,50 +85,55 @@ const Index = ({ productId, reviews }: IProps) => {
   return (
     <div className="mx-auto max-w-[700px] px-2">
       <h2 className="text-xl font-semibold">Ratings and Reviews</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {errors.root?.message && <Alert type="error" message={errors.root.message} />}
-        <div className="mt-3">
-          <div className="mb-2">Please rate the product to add a review</div>
-          {[1, 2, 3, 4, 5].map((i) => {
-            const rating = watch('rating');
-            const active = i <= rating;
+      {Boolean(userCanReview) && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {errors.root?.message && <Alert type="error" message={errors.root.message} />}
+          <div className="mt-3">
+            <div className="mb-2">Please rate the product to add a review</div>
+            {[1, 2, 3, 4, 5].map((i) => {
+              const rating = watch('rating');
+              const active = i <= rating;
 
-            return (
-              <button
-                key={i}
-                type="button"
-                className=""
-                onClick={() => handleClick(i)}
-                onMouseEnter={() => handleHover(i)}
-                onMouseLeave={() => handleHover(rating)}>
-                <span className="sr-only">{i} star rating</span>
-                {active ? (
-                  <ActiveStarIcon className="h-6 w-6 text-yellow-500" />
-                ) : (
-                  <StarIcon className="h-6 w-6" />
-                )}
-              </button>
-            );
-          })}
-          {errors.rating?.message}
-        </div>
-        <TextArea
-          id="comment"
-          label="Write a Review"
-          placeholder="Write a new review about this product..."
-          rows={4}
-          control={control}
-          error={errors.comment}
-        />
-        <Button type="submit" className="bg-primary-500 my-2 text-white" isLoading={isSubmitting}>
-          Add a Review
-        </Button>
-      </form>
+              return (
+                <button
+                  key={i}
+                  type="button"
+                  className=""
+                  onClick={() => handleClick(i)}
+                  onMouseEnter={() => handleHover(i)}
+                  onMouseLeave={() => handleHover(rating)}>
+                  <span className="sr-only">{i} star rating</span>
+                  {active ? (
+                    <ActiveStarIcon className="h-6 w-6 text-yellow-500" />
+                  ) : (
+                    <StarIcon className="h-6 w-6" />
+                  )}
+                </button>
+              );
+            })}
+            {errors.rating?.message}
+          </div>
+          <TextArea
+            id="comment"
+            label="Write a Review"
+            placeholder="Write a new review about this product..."
+            rows={4}
+            control={control}
+            error={errors.comment}
+          />
+          <Button type="submit" className="bg-primary-500 my-2 text-white" isLoading={isSubmitting}>
+            Add a Review
+          </Button>
+        </form>
+      )}
       <div className="my-2 space-y-2">
-        {reviews.map(({ _id, name, comment, rating }) => (
+        {reviews.map(({ _id, name, comment, rating, user: ratedUser }) => (
           <div key={_id} className="rounded-xl py-4">
             <div className="text-lg font-semibold">
               {name}{' '}
+              {user?._id === ratedUser && (
+                <sub className="rounded-xl bg-blue-400 p-1 text-xs text-white">You</sub>
+              )}
               <span className="text-primary-600 ml-2 border-l-2 border-slate-500 pl-2 text-sm font-medium">
                 Verified Purchase
               </span>
